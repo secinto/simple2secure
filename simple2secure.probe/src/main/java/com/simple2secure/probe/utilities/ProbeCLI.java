7cb0ea518c975587cd6087f0b84004ddfb11a88a
velicben <benjamin.velic@secinto.com>
2018-11-14 16:15:48 +0100

    [artf154419] created ProbeCLI Application

--- simple2secure.probe/src/main/java/com/simple2secure/probe/utilities/ProbeCLI.java
+++ simple2secure.probe/src/main/java/com/simple2secure/probe/utilities/ProbeCLI.java
@@ -1,27 +1,37 @@
 package com.simple2secure.probe.utilities;
 
+import java.util.Scanner;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.simple2secure.api.model.CompanyLicenseObj;
+import com.simple2secure.probe.gui.ProbeWorkerThread;
 
 public class ProbeCLI {
-	private static Logger log = LoggerFactory.getLogger(ImportFileManager.class);
+	private static Logger log = LoggerFactory.getLogger(ProbeCLI.class);
 
+	public void start() {
+	    boolean running = true;
 
+	}
+	
 	public static void main(String[] args) {
 		String importFilePath;
-		
-		if(args.length != 2) {
+
+		if (args.length != 2) {
 			log.error("Invalid amount of parameters.");
-		}
-		else {
+		} else {
 			importFilePath = args[1].toString();
-				
-			CompanyLicenseObj licenseFile = ProbeUtilities.loadLicenseFromPath("K:\\work\\projects\\simple2secure\\license-5be43bd01dbe9510b818e4de.zip");
-			
-			if (licenseFile != null)
+
+			CompanyLicenseObj licenseFile = ProbeUtilities.loadLicenseFromPath(importFilePath);
+
+			if (licenseFile != null) {
 				log.debug("The license file was imported succesfully.");
+			}
 		}
+
+		ProbeWorkerThread workerThread = new ProbeWorkerThread();
+		workerThread.run();
 	}
 }
--- simple2secure.probe/src/main/java/com/simple2secure/probe/utils/RequestHandler.java
+++ simple2secure.probe/src/main/java/com/simple2secure/probe/utils/RequestHandler.java
@@ -3,6 +3,7 @@ package com.simple2secure.probe.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
@@ -13,60 +14,56 @@ import com.simple2secure.probe.config.ProbeConfiguration;
 public class RequestHandler {
 
 	private final static Logger log = LoggerFactory.getLogger(RequestHandler.class);
-
 	final static String BEARER = "Bearer ";
-	private static String output;
 
+	private RequestHandler() {}
+	
 	public static void sendPost(String url_, Object obj) {
-		String response;
-		try {
-			response = createPostRequest(url_, obj);
-			log.debug("Response from POST reques: " + response);
-		}catch(UnirestException ex) {
-			ProbeConfiguration.setAPIAvailablitity(false);
-			log.error("Couldn 't send POST to URL {} because of execption {}", url_, ex.getStackTrace());
-		}
-		ProbeConfiguration.setAPIAvailablitity(true);
-
+		sendPostReceiveResponse(url_, obj);
 	}
 	
 	public static String sendPostReceiveResponse(String url_, Object obj) {
-		String response = new String();
+		ObjectMapper oM = new ObjectMapper();
+		String requestBody = new String();
+		String response = "";
+		
 		try {
-			response = createPostRequest(url_, obj);
+			requestBody = oM.writeValueAsString(obj);
+			HttpResponse<String> jsonResponse = createPostRequest(url_, requestBody);
+			response = jsonResponse.getBody();
 			log.debug("Response from POST request: " + response);
-		}catch(UnirestException ex) {
+		}catch(UnirestException | JsonProcessingException ex) {
 			ProbeConfiguration.setAPIAvailablitity(false);
-			log.error("Couldn 't send POST to URL {} because of execption {}", url_, ex.getStackTrace());
+			log.error("Couldn 't send POST to URL {} because of execption {}", url_, ex);
 		}
 		ProbeConfiguration.setAPIAvailablitity(true);
 		return response;
 	}
 
-	public static String sendGet(String url_) {
-		String response = new String();
+	public static String sendGet(String url_) {	
+		String response = "";
 		try {
-			response = createGetRequest(url_);
+			HttpResponse<String> jsonResponse = createGetRequest(url_);
+			response = jsonResponse.getBody();
+			log.debug("Response from POST request: " + response);
+			
 		} catch (UnirestException ex) {
 			ProbeConfiguration.setAPIAvailablitity(false);
 			log.error("Couldn 't send GET to URL {} with response because of execption {}", url_, ex.getStackTrace());
 		}
 		ProbeConfiguration.setAPIAvailablitity(true);
 		return response;
-
 	}
 
-	private static String createPostRequest(String url, Object object) throws UnirestException {
-		ObjectMapper mapper = new ObjectMapper();
-		HttpResponse<JsonNode> jsonResponse = Unirest.post(url).header("Content-Type", "application/json").
-				header("Authorization", BEARER + ProbeConfiguration.authKey).header("Accept-Language", "en").body(object).asJson();
-		return jsonResponse.toString();
+	private static HttpResponse<String> createPostRequest(String url, String object) throws UnirestException {
+		HttpResponse<String> jsonResponse = Unirest.post(url).header("Content-Type", "application/json").
+				header("Authorization", BEARER + ProbeConfiguration.authKey).header("Accept-Language", "en").body(object).asString();
+		return jsonResponse;
 	}
 	
-	private static String createGetRequest(String url) throws UnirestException {
-		ObjectMapper mapper = new ObjectMapper();
-		HttpResponse<JsonNode> jsonResponse = Unirest.get(url).header("Content-Type", "application/json").
-				header("Authorization", BEARER + ProbeConfiguration.authKey).header("Accept-Language", "en").asJson();
-		return jsonResponse.toString();
+	private static HttpResponse<String> createGetRequest(String url) throws UnirestException {
+		HttpResponse<String> jsonResponse = Unirest.get(url).header("Content-Type", "application/json").
+				header("Authorization", BEARER + ProbeConfiguration.authKey).header("Accept-Language", "en").asString();
+		return jsonResponse;
 	}
 }
