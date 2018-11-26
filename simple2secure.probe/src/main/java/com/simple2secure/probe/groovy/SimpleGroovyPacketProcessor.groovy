0c137cffb1efc93b2ad84ac817933d07e643969f
Stefan Kraxberger <stefan.kraxberger@secinto.com>
2018-11-26 08:04:19 +0100

    [artf155317] Modifications for license manager

--- simple2secure.probe/src/main/java/com/simple2secure/probe/groovy/SimpleGroovyPacketProcessor.groovy
+++ simple2secure.probe/src/main/java/com/simple2secure/probe/groovy/SimpleGroovyPacketProcessor.groovy
@@ -1,6 +1,7 @@
 package com.simple2secure.probe.groovy
 
 import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 import com.simple2secure.probe.network.PacketContainer
 import com.simple2secure.probe.network.PacketProcessor
