34ee4205b51fc9d6ee6fe4b6881d58778db659c2
stefan.kraxberger@secinto.com <stefan.kraxberger@secinto.com>
2018-11-30 15:38:33 +0100

    [artf155757] Service tests finally working

--- simple2secure.service/src/test/java/com/simple2secure/service/test/TestProcessUtils.java
+++ simple2secure.service/src/test/java/com/simple2secure/service/test/TestProcessUtils.java
@@ -3,6 +3,7 @@ package com.simple2secure.service.test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,18 +16,12 @@ public class TestProcessUtils {
 	private CountDownLatch lock = new CountDownLatch(1);
 
 	@Test
-	public void testDemoApplicationStart() throws Exception {
-		ProcessUtils.invokeJavaProcess(null, false, "-cp", "../../release/simple2secure.service-0.1.0.jar",
-				"com.simple2secure.service.test.EchoClient", "localhost", "8000");
-	}
-
-	@Test
 	public void testCreateProcess() throws Exception {
-		ProcessContainer container = ProcessUtils.createProcess("cmd.exe", "/c", "java", "-version");
 		TestLoggingObserver observer = new TestLoggingObserver();
+		ProcessContainer container = ProcessUtils.createProcess("cmd.exe", "/c", "java", "-version");
 		container.getObservable().addObserver(observer);
 		container.startObserving();
-		// lock.await(2000, TimeUnit.MILLISECONDS);
+		lock.await(2000, TimeUnit.MILLISECONDS);
 		assertTrue(observer.getFirstObservable().contains("java version"));
 		assertTrue(observer.getLastObservable().contains("Java HotSpot"));
 
