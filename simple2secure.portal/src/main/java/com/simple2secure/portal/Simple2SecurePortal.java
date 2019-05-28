dbc57a06fab59df1efebd5541442148f98720fae
Emir Sahinovic <emir.sahinovic@secinto.com>
2019-05-28 09:43:05 +0200

    Minor updates to check if works on the hetzner

--- simple2secure.portal/src/main/java/com/simple2secure/portal/Simple2SecurePortal.java
+++ simple2secure.portal/src/main/java/com/simple2secure/portal/Simple2SecurePortal.java
@@ -35,7 +35,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import com.simple2secure.commons.config.LoadedConfigItems;
-import com.simple2secure.commons.config.StaticConfigItems;
 import com.simple2secure.portal.repository.ServiceLibraryRepository;
 
 @EnableScheduling
@@ -132,13 +131,11 @@ public class Simple2SecurePortal extends SpringBootServletInitializer {
 	public void currentActiveProfile() {
 		String[] activeProfiles = env.getActiveProfiles();
 
-		for (String profile : activeProfiles) {
-			if (profile.equals(StaticConfigItems.PROFILE_PRODUCTION)) {
-				LoadedConfigItems loadedConfigItems = LoadedConfigItems.getInstance();
-				loadedConfigItems.setBasePort("8443");
-				loadedConfigItems.setBaseProtocol("https");
-			}
-		}
+		/*
+		 * for (String profile : activeProfiles) { if (profile.equals(StaticConfigItems.PROFILE_PRODUCTION)) { LoadedConfigItems
+		 * loadedConfigItems = LoadedConfigItems.getInstance(); loadedConfigItems.setBasePort("8443");
+		 * loadedConfigItems.setBaseProtocol("https"); loadedConfigItems.setBaseHost("localhost"); } }
+		 */
 	}
 
 	public static void main(String[] args) throws IOException {
