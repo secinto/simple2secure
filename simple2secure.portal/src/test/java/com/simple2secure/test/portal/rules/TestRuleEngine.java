9123dde38a8e5d4c1f1d62a0c136495a164baae6
richard <richard.heinz@secinto.at>
2019-08-23 14:32:24 +0200

    added selfmade annotation for the rule condition params

--- simple2secure.portal/src/test/java/com/simple2secure/test/portal/rules/TestRuleEngine.java
+++ simple2secure.portal/src/test/java/com/simple2secure/test/portal/rules/TestRuleEngine.java
@@ -2,9 +2,14 @@ package com.simple2secure.test.portal.rules;
 
 
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.jeasy.rules.api.Condition;
+import org.jeasy.rules.api.Facts;
+import org.jeasy.rules.api.Rule;
+import org.jeasy.rules.core.RuleBuilder;
 import org.joda.time.DateTime;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,28 +18,50 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.google.common.reflect.ClassPath;
 import com.google.gson.Gson;
 import com.simple2secure.api.model.Email;
 import com.simple2secure.api.model.EmailAttribute;
 import com.simple2secure.api.model.EmailAttributeEnum;
-import com.simple2secure.commons.rules.GeneralRulesEngine;
-import com.simple2secure.commons.rules.GeneralRulesEngineImpl;
+import com.simple2secure.api.model.TemplateRule;
+import com.simple2secure.commons.rules.annotations.ConditionParam;
+import com.simple2secure.commons.rules.annotations.ConditionParamArray;
+import com.simple2secure.commons.rules.engine.GeneralRulesEngine;
+import com.simple2secure.commons.rules.engine.GeneralRulesEngineImpl;
 import com.simple2secure.portal.Simple2SecurePortal;
+import com.simple2secure.portal.dao.MongoRepository;
 import com.simple2secure.portal.repository.EmailAttributesListRepository;
+import com.simple2secure.portal.rules.EmailRulesEngine;
 import com.simple2secure.portal.rules.PortalRulesEngine;
+import com.simple2secure.portal.rules.TemplateActionSendNotification;
+import com.simple2secure.portal.rules.TemplateEmailCondition;
+import com.simple2secure.portal.rules.TemplateRuleBuilder;
+import com.simple2secure.portal.rules.TemplateConditionFindWords;
+import com.simple2secure.portal.rules.TemplateConditionSpecificDomains;
 
-@ExtendWith({ SpringExtension.class })
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = { Simple2SecurePortal.class })
-@ActiveProfiles("test")
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
+//@ExtendWith({ SpringExtension.class })
+//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = { Simple2SecurePortal.class })
+//@ActiveProfiles("test")
 public class TestRuleEngine {
 	
+	@Autowired
+	private TemplateRuleBuilder templateRuleBuilder;
+	
+	/*
 	@Autowired
 	EmailAttributesListRepository emailAttributesListRepository;
 	
 	@Autowired
 	PortalRulesEngine portalRulesEngine;
-
 	
+	@Autowired EmailRulesEngine emailRulesEngine;
+*/
+	
+	/*
 	@Test
 	public void testFillEmailAttributeRepository()
 	{		
@@ -68,22 +95,39 @@ public class TestRuleEngine {
 		}
 	}
 	
+	
+
+	private Email[] readJsonMails()
+	{		
+		String emails_directory = "C:\\Users\\Richard Heinz\\Desktop\\test_emails.json";
+		
+		try ( final FileReader fileReader = new FileReader(emails_directory) ){
+			Email[] emails = new Gson().fromJson(fileReader, Email[].class);
+	        
+			return emails;
+	    }
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	@Test void testEmailRulesEngine()
+	{
+		Email[] emails = readJsonMails();
+		
+		for(Email email : emails)
+		{
+			emailRulesEngine.addFact(email);
+		}
+	}
+	
+	/*
 	@Test
 	public void testPortalRuleEngine()
-	{
-		Collection<Email> mails = new ArrayList<Email>();
-		
-		Email mail1 = new Email();
-		mail1.setFrom("alice@hacker.com");
-		mail1.setSubject("asdf");
-		mail1.setText("some textDOWNLOAD");
-		mails.add(mail1);
-		
-		Email mail2 = new Email();
-		mail2.setFrom("normal1@normal.com");
-		mail2.setSubject("asdf");
-		mail2.setText("some text");
-		mails.add(mail2);
+	{	
 		
 		try {			
 			String email_rule_as_string1 = new Gson().fromJson(new FileReader(
@@ -108,6 +152,7 @@ public class TestRuleEngine {
 			e.printStackTrace();
 		}
 	}
+	
 	
 	@Test
 	public void testGeneralRuleEngine() 
@@ -137,4 +182,103 @@ public class TestRuleEngine {
 			e.printStackTrace();
 		}
     }
+	
+	*/
+	
+	public org.jeasy.rules.api.Rule testRuleTemplate() // only for testing
+	{
+		TemplateRule templateRule = new TemplateRule();	
+		templateRule.setName("testRule");
+		templateRule.setDescription("rule to test");
+		templateRule.getContextID(); // todo
+		templateRule.setConditionTemplate("find_words");
+		String[] needles = {"test", "spam"};
+		String[] domains = {"secinto.at", "gmail.com"};
+		templateRule.setConditionParams(needles);
+		templateRule.setActionTemplate("send_notification");
+		String[] params = {"test worked fine!"};
+		//String[] params = {"test worked fine!",  "richard.heinz@secinto.at"};
+		templateRule.setActionParams(params);
+		return templateRuleBuilder.build(templateRule);
+	}
+	
+	
+	@Test
+	public void testConditionWithAnnotations()
+	{
+		String[] words = {"abc", "def"};
+		TemplateConditionFindWords con = new TemplateConditionFindWords(words);
+
+		Class clazz = con.getClass();
+		
+		Field fs[] = clazz.getDeclaredFields();
+		for (Field f : fs) 
+	    {
+			f.setAccessible(true);
+	    	System.out.println(f);
+
+		    
+		    if(f.getAnnotation(ConditionParamArray.class) instanceof ConditionParamArray)
+		    {
+		    	ConditionParamArray paramArray = (ConditionParamArray) f.getAnnotation(ConditionParamArray.class);
+		    	
+		    	System.out.println(paramArray.description_de());
+		    	
+		    	try {
+		    		String[] array = {"new",  "words", "in", "array"};
+					f.set(con, array);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    }
+		    
+		    if(f.getAnnotation(ConditionParam.class) instanceof ConditionParam)
+		    {
+		    	ConditionParam param = (ConditionParam) f.getAnnotation(ConditionParam.class);
+		    	
+		    	System.out.println(param.description_de());
+		    	System.out.println(param.name());
+		    	
+		    	try {
+			    	if(param.name().contentEquals("limited word"))
+			    		f.set(con, "new_word");
+
+			    	
+			    	if(param.name().contentEquals("max word count"))
+			    		f.set(con, 100);
+					
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    }
+	    }
+	}
+	
+	@Test
+	public void testLoadingClassesFromPackage()
+	{
+		TemplateActionSendNotification temp = new TemplateActionSendNotification("asdf");
+		ClassLoader loader = temp.getClass().getClassLoader();
+	    ClassPath path;
+		try {
+			path = ClassPath.from(loader);
+			for (ClassPath.ClassInfo info : path.getTopLevelClassesRecursive("com.simple2secure.test.portal.rules")) {
+		        try {
+					Class clazz = Class.forName(info.getName(), true, loader);
+					System.out.println(clazz.getName());
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        //dowhatever
+		    }
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
 }
+
+
