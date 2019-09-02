31251aeca38534a19d0568dee67481af843b7a96
Stefan Kraxberger <stefan.kraxberger@secinto.com>
2019-09-02 12:17:20 +0200

    [artf155317] Clean up from merge from release-1.0 (some files have not been removed - need to check why so that in future it doesn't happen)

--- simple2secure.portal/src/main/java/com/simple2secure/portal/security/CustomEntryPoint.java
+++ simple2secure.portal/src/main/java/com/simple2secure/portal/security/CustomEntryPoint.java
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-package com.simple2secure.portal.security;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.stereotype.Component;
-
-@Component
-public class CustomEntryPoint implements AuthenticationEntryPoint {
-
-	@Override
-	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
-			throws IOException, ServletException {
-
-		response.setContentType("application/json");
-		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-		response.getOutputStream().println(
-				"{ \"error\": \"" + authException.getMessage() + "\" ,\"message\": \" User with provided credentials does not exists \" }");
-	}
-
-}
-=======
 /**
  *********************************************************************
  *   simple2secure is a cyber risk and information security platform.
@@ -73,4 +45,3 @@ public class CustomEntryPoint implements AuthenticationEntryPoint {
 	}
 
 }
->>>>>>> release-1.0
--- simple2secure.portal/src/test/java/com/simple2secure/test/portal/token/TestJWTToken.java
+++ simple2secure.portal/src/test/java/com/simple2secure/test/portal/token/TestJWTToken.java
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-package com.simple2secure.test.portal.token;
-
-import java.util.Date;
-
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.common.base.Strings;
-import com.simple2secure.portal.security.auth.CustomAuthenticationProvider;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-
-public class TestJWTToken {
-
-	private static Logger log = LoggerFactory.getLogger(TestJWTToken.class);
-
-	static final long EXPIRATIONTIME = 864000000; // 10 days
-	static final String SECRET = "ThisIsASecret";
-	static final String TOKEN_PREFIX = "Bearer";
-	static final String HEADER_STRING = "Authorization";
-
-	@Test
-	public void testJWTTokenDate() throws Exception {
-		Claims claims = Jwts.claims().setSubject("test");
-		claims.put("userID", CustomAuthenticationProvider.userID);
-
-		long currentDate = System.currentTimeMillis();
-
-		log.info("CurrentDate {}", currentDate);
-		log.info("CurrentDate + Expiration {}", currentDate + EXPIRATIONTIME);
-		log.info("Date {}", new Date(currentDate + EXPIRATIONTIME));
-
-		String JWT = Jwts.builder().setClaims(claims).setSubject("testuser").setExpiration(new Date(currentDate + EXPIRATIONTIME))
-				.signWith(SignatureAlgorithm.ES512, SECRET).compact();
-
-		if (!Strings.isNullOrEmpty(JWT)) {
-			log.info("JWT Token {}", JWT);
-		}
-	}
-}
-=======
 /**
  *********************************************************************
  *   simple2secure is a cyber risk and information security platform.
@@ -109,4 +64,3 @@ public class TestJWTToken {
 		}
 	}
 }
->>>>>>> release-1.0
