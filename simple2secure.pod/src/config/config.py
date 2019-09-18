acfeec90ec4201fcfd945ca39593952b7c2f932b
Stefan Kraxberger <stefan.kraxberger@secinto.com>
2019-09-18 12:32:40 +0200

    Updates for current version, which does not accept Marshmallow schema.dump(something).data. Modified config and tried to fix template issue, not finished yet.

--- simple2secure.pod/src/config/config.py
+++ simple2secure.pod/src/config/config.py
@@ -4,7 +4,6 @@ import urllib3
 class Config(object):
 
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    DEBUG = False
     TESTING = False
     SECRET_KEY = "ChangeIt2019!"
     DATABASE_URI = 'sqlite:///:memory:'
@@ -14,18 +13,19 @@ class Config(object):
     POD_ID = ''
     GROUP_ID = ''
     LICENSE_ID = ''
-    # PORTAL_URL = 'https://localhost:8443/api/'
+    #PORTAL_URL = 'https://localhost:8443/api/'
     PORTAL_URL = 'https://144.76.93.104:51001/s2s/api/'
     AUTH_TOKEN = ''
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
+    DEBUG = False
     DATABASE_URI = 'mysql://user@localhost/foo'
     CELERY_BROKER_URL = "redis://redis:6379"
     CELERY_RESULT_BACKEND = "redis://redis:6379"
 
 
 class DevelopmentConfig(Config):
-    DEBUG = False
+    DEBUG = True
 
