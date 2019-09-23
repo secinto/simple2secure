65813d9add185b336c934eb782906a279aac14b9
Stefan Kraxberger <stefan.kraxberger@secinto.com>
2019-09-23 08:30:48 +0200

    Cleanup

--- simple2secure.web/src/app/_models/queryconfig.ts
+++
@@ -1,7 +0,0 @@
-import {Generic} from './generic';
-
-export class QueryConfig extends Generic {
-  querystrings: string;
-  queryconfig: string;
-  osquerypath: string;
-}
--- simple2secure.web/src/app/gui/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './gui.component';
-export * from './guiOverview.component';
-export * from './guiImport.component';
-export * from './guiUserOverview.component';
