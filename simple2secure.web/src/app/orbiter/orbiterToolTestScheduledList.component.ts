00ca1aada68eab21ac56fcfaf272da1d7e42d7a0
Stefan Kraxberger <stefan.kraxberger@secinto.com>
2019-09-23 08:45:12 +0200

    Minor structural changes

--- simple2secure.web/src/app/orbiter/orbiterToolTestScheduledList.component.ts
+++ simple2secure.web/src/app/orbiter/orbiterToolTestScheduledList.component.ts
@@ -1,3 +1,25 @@
+/**
+ *********************************************************************
+ *   simple2secure is a cyber risk and information security platform.
+ *   Copyright (C) 2019  by secinto GmbH <https://secinto.com>
+ *********************************************************************
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *********************************************************************
+ */
+
 import {Component, ViewChild} from '@angular/core';
 import {MatDialog, MatDialogConfig, MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {TranslateService} from '@ngx-translate/core';
@@ -136,8 +158,8 @@ export class OrbiterToolTestScheduledListComponent {
 				this.loading = false;
 			});
 	}
-
-	openDialogShowTestResult(): void {
+	
+	public openDialogShowTestResult(): void {
 		const dialogConfig = new MatDialogConfig();
 		dialogConfig.width = '450px';
 		dialogConfig.data = {
@@ -148,7 +170,4 @@ export class OrbiterToolTestScheduledListComponent {
 
 	}
 
-	getTestStatusByTestResult(element: any) {
-
-	}
 }
