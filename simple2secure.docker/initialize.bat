014394b26760ebebb00fe383453afde13d771b37
Emir Sahinovic <emir.sahinovic@secinto.com>
2019-10-25 12:35:58 +0200

    Updates on the initialize.bat and readme. Option to skip git checkout

--- simple2secure.docker/initialize.bat
+++ simple2secure.docker/initialize.bat
@@ -4,12 +4,21 @@ title simple2secure Docker
 echo **************************************************************
 echo *** Initializing simple2secure pod and standard tool chain ***
 echo **************************************************************
+echo Would you like to skip git checkout (yes/no)?
+set /p choice= "Please Select one of the above options :" 
+if %choice%==yes goto RUNDOCKER
+else goto GITCHECKOUT
+
+:GITCHECKOUT
 echo Deleting %directory% directory
 rmdir /s /q %directory%
 echo Creating empty %directory% directory!
 mkdir %directory%
 echo Cloning simple2secure project in the %directory% directory...
 git clone https://github.com/secinto/simple2secure.git %directory%
+goto RUNDOCKER
+
+:RUNDOCKER
 echo Installing python libraries
 pip install -r requirements.txt
 echo Starting python script for retrieving license
