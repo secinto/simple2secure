5717bdef9ab419ab335aa6fa69d77201b5fbfc5c
Stefan <stefan.kraxberger@secinto.com>
2019-10-25 18:59:10 +0200

    removal

--- simple2secure.info/createP12.sh
+++ simple2secure.info/createP12.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cp /etc/letsencrypt/live/simple2secure.info/*.pem .
 openssl pkcs12 -export -in nginx.pem -inkey nginx.key -chain -CAfile chain.crt -name "tomcat" -out keystore.p12
+rm *.pem
