30fa7ae6038a79175313467b71f64ce01261ed02
secinto <office@secinto.com>
2019-10-28 09:49:16 +0100

    Modifications for correct publishing

--- simple2secure.info/createP12.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp /etc/letsencrypt/live/simple2secure.info/*.pem .
-openssl pkcs12 -export -in nginx.pem -inkey nginx.key -chain -CAfile chain.crt -name "tomcat" -out keystore.p12
-rm *.pem
