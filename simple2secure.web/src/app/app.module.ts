e584a091d4332d257ac2757fe854d44c7c8129fb
secinto <office@secinto.com>
2019-11-20 10:52:32 +0100

    Revert "Removed statcomponent from app"
    This reverts commit 994a1681fa9b94dd79a310417358747104ac3764.

--- simple2secure.web/src/app/app.module.ts
+++ simple2secure.web/src/app/app.module.ts
@@ -174,6 +174,7 @@ import { NgxWidgetGridModule } from 'ngx-widget-grid';
 import { BoxModule } from 'angular-admin-lte';
 import { SUTDetailsComponent } from './orbiter/sutDetails.component';
 import {BreadcrumbsModule} from 'ng6-breadcrumbs';
+import {StatComponent} from './widgets/stat.component';
 import {NotificationCardComponent} from './widgets/notification-card.component';
 import {WidgetStoreComponent} from './widgets/widgetStore.component';
 import {StatItemComponent} from './widgets/stat-item.component';
@@ -329,6 +330,7 @@ export const httpInterceptorProviders = [
 		TestSequenceResultDetailsComponent,
 		OrbiterSystemsUnderTestListComponent,
 		SUTDetailsComponent,
+		StatComponent,
 		StatItemComponent,
 		NotificationCardComponent,
 		WidgetStoreComponent
@@ -358,6 +360,7 @@ export const httpInterceptorProviders = [
 		TestSequenceResultDetailsComponent,
 		WidgetStoreComponent,
 		StatItemComponent,
+		StatComponent,
 		SUTDetailsComponent
 	],
 	providers: [
