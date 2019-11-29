a79865252544b6a11b7c7b61d73d57e8bad3379e
Emir Sahinovic <emir.sahinovic@secinto.com>
2019-11-29 08:51:14 +0100

    ValidInputName forgot to add model

---
+++ simple2secure.validator/src/main/java/simple2secure/validator/model/ValidInputName.java
@@ -0,0 +1,34 @@
+package simple2secure.validator.model;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+public class ValidInputName extends ValidatedInput<String>{
+
+	private String name;
+	private String tag = "/{name}";
+	
+	public ValidInputName() {
+	}
+	
+	public ValidInputName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String getValue() {
+		return name;
+	}
+
+	@Override
+	public String getTag() {
+		return tag;
+	}
+
+	@Override
+	public Object validate(Authentication auth, MethodParameter methodParameter, HttpServletRequest request) {
+		//TODO: check how to use the repository
+		return null;
+	}
+}
