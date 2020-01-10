1be4ceefcb89742993e603412774142e1ad7f882
rheinz <richard.heinz@secinto.at>
2020-01-10 16:03:10 +0100

    added license comment

--- simple2secure.api/src/main/java/com/simple2secure/api/model/TriggeredRuleEmail.java
+++ simple2secure.api/src/main/java/com/simple2secure/api/model/TriggeredRuleEmail.java
@@ -1,3 +1,25 @@
+/**
+*********************************************************************
+*   simple2secure is a cyber risk and information security platform.
+*   Copyright (C) 2019  by secinto GmbH <https://secinto.com>
+*********************************************************************
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU Affero General Public License as
+*   published by the Free Software Foundation, either version 3 of the
+*   License, or (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*   GNU Affero General Public License for more details.
+*
+*   You should have received a copy of the GNU Affero General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*  
+ *********************************************************************
+*/
+
 package com.simple2secure.api.model;
 
 import java.util.ArrayList;
@@ -8,22 +30,22 @@ import com.simple2secure.api.dbo.GenericDBObject;
 import lombok.Getter;
 
 @Getter
-public class TriggeredRule extends GenericDBObject {
+public class TriggeredRuleEmail extends GenericDBObject {
 	
 	private static final long serialVersionUID = -5472051873781398191L;
 	private TemplateRule rule;
 	private List<Email> emails;	
 	
-	public TriggeredRule() {
+	public TriggeredRuleEmail() {
 		
 	}
 	
-	public TriggeredRule(TemplateRule rule, List<Email> emails) {
+	public TriggeredRuleEmail(TemplateRule rule, List<Email> emails) {
 		this.rule = rule;
 		this.emails = emails;
 	}
 	
-	public TriggeredRule(TemplateRule rule)
+	public TriggeredRuleEmail(TemplateRule rule)
 	{
 		this.rule = rule;
 		emails = new ArrayList<>();
--- simple2secure.portal/src/main/java/com/simple2secure/portal/repository/impl/EmailTriggeredRuleHistoryRepositoryImpl.java
+++ simple2secure.portal/src/main/java/com/simple2secure/portal/repository/impl/EmailTriggeredRuleHistoryRepositoryImpl.java
@@ -1,3 +1,25 @@
+/**
+*********************************************************************
+*   simple2secure is a cyber risk and information security platform.
+*   Copyright (C) 2019  by secinto GmbH <https://secinto.com>
+*********************************************************************
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU Affero General Public License as
+*   published by the Free Software Foundation, either version 3 of the
+*   License, or (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*   GNU Affero General Public License for more details.
+*
+*   You should have received a copy of the GNU Affero General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*  
+ *********************************************************************
+*/
+
 package com.simple2secure.portal.repository.impl;
 
 import javax.annotation.PostConstruct;
@@ -7,25 +29,25 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.simple2secure.api.model.TriggeredRule;
-import com.simple2secure.portal.repository.EmailRuleTriggeredRepository;
+import com.simple2secure.api.model.TriggeredRuleEmail;
+import com.simple2secure.portal.repository.EmailTriggeredRuleHistoryRepository;
 
 @Repository
 @Transactional
-public class EmailRuleTriggeredRepositoryImpl extends EmailRuleTriggeredRepository{
+public class EmailTriggeredRuleHistoryRepositoryImpl extends EmailTriggeredRuleHistoryRepository{
 
 	@PostConstruct
 	public void init() {
-		super.collectionName = "triggeredRule";
-		super.className = TriggeredRule.class;
+		super.collectionName = "triggeredRuleEmail";
+		super.className = TriggeredRuleEmail.class;
 	}
 	
 	@Override
-	public TriggeredRule findByRuleName(String ruleName) {
+	public TriggeredRuleEmail findByRuleName(String ruleName) {
 		// TODO Auto-generated method stub
 		
 		Query query = new Query(Criteria.where("rule.name").is(ruleName));
-		return mongoTemplate.findOne(query, TriggeredRule.class, collectionName);
+		return mongoTemplate.findOne(query, TriggeredRuleEmail.class, collectionName);
 	}
 
 	@Override
