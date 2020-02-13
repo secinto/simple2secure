f3922c6be7d57d22b857d9631e4f39d4d67657bb
rheinz <richard.heinz@secinto.at>
2020-02-13 13:58:42 +0100

    changed membervariables

--- simple2secure.web/src/app/_models/ruleParamArray.ts
+++ simple2secure.web/src/app/_models/ruleParamArray.ts
@@ -1,9 +1,8 @@
 import {DataType} from './dataType';
 
 export class RuleParamArray<T> {
-    name: string;
-    description_en: string;
-    description_de: string;
+    nameTag: string;
+    descriptionTag: string;
     values: T[];
     type: DataType;
 }
